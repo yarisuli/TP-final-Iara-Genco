bfc65c7d00eac1c1a5ea8a65d685a20d57da678b
yarisuli <148562678+yarisuli@users.noreply.github.com>
2024-06-24 22:32:15 -0300

    Initial commit

---
+++ Assets/scripts/movement_RL.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement_RL : MonoBehaviour
+{
+    public GameObject[] objetos;
+    int indice = 0;
+    void Start()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            objetos[indice].SetActive(false);
+            indice--;
+            //if (indice < 0)
+            //{
+            //    indice = objetos.Length;
+            //}
+            objetos[indice].SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            objetos[indice].SetActive(false);
+            indice++;
+            //if (indice > objetos.Length)
+            //{
+            //    indice = 0;
+            //}
+            objetos[indice].SetActive(true);
+        }
+    }
+}
